4f14bac20ccc18751e7a4e488b091dddfaf52eca
Rishit Ratna <rishitratan5@gmail.com>
2024-01-22 23:18:09 -0700

    Time: 661 ms (5.04%), Space: 44.4 MB (55.54%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,11 +1,15 @@
 class Solution {
     public int maxLength(List<String> arr) {
+        // ["a", "b", "c", "a"]
+        // [ 1 ,  1 ,  1 ,  0] => 3 
+        // [ 1 ,  1 ,  1 ,  1] => Binary equivalent of 2
+        // [ 0 ,  0 ,  1 ,  1] => Binary equivalent of 0
         int maxLen = 0;
-        for (int i = 0; i < Math.pow(2, arr.size()); i++) {
+        for (int i = 0; i < Math.pow(2, arr.size()); i++) { // 0, 1, 2, 3, ... 15 
             int flag = 0;
             Set<Character> set = new HashSet<>();
-            for (int j = 0; j < arr.size(); j++) {
-                if ((i >> j & 1) == 1) {
+            for (int j = 0; j < arr.size(); j++) {  // j = 0, 1, 2, 3
+                if ((i >> j & 1) == 1) {            // i = 00000000000011 & 0000000000001 => 1
                     for (char c : arr.get(j).toCharArray()) {
                         if (set.contains(c)) {
                             flag = 1;
@@ -23,3 +27,4 @@ class Solution {
         return maxLen;
     }
 }
+
