fdf6fbc44970404983ffe6fd561b0b66ea04f164
Rishit Ratna <rishitratan5@gmail.com>
2024-07-19 23:33:25 -0700

    Time: 1 ms (100.00%), Space: 52.8 MB (72.91%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -1,40 +1,18 @@
 class Solution {
-    int[][] result;
-    private int[] next(int i, int j, int rowSumLength, int colSumLength) {
-        if (j+1 == colSumLength) {
-            int[] result = {i+1, 0};
-            return result;
-        } else {
-            int[] result = {i, j+1};
-            return result;
-        }
-    }
-    private boolean calculate(int[] rowSum, int[] colSum, int i, int j) {
-        if (i == rowSum.length) {
-            return true;
-        } else {
-            for (int k = 0; k < Math.min(rowSum[i], colSum[j]); k++) {
-                result[i][j] = k;
-                rowSum[i] = rowSum[i] - k;
-                colSum[j] = colSum[j] - k;
-                int[] lengths = next(i, j, rowSum.length, colSum.length);
-                if (calculate(rowSum, colSum, lengths[0], lengths[1]) == true) {
-                    return true;
-                }
-                result[i][j] = 0;
-                rowSum[i] = rowSum[i] + k;
-                colSum[j] = colSum[j] + k;
-            }
-        }
-        return false;
-    }
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int[][] result = new int[rowSum.length][colSum.length];
-        for (int i = 0; i < rowSum.length; i++) {
-            for (int j = 0; j < colSum.length; j++) {
-                result[i][j] = Math.min(rowSum[i], colSum[j]);
-                rowSum[i] = rowSum[i] - result[i][j];
-                colSum[j] = colSum[j] - result[i][j];
+        int i = 0, j = 0;
+        int n = rowSum.length;
+        int m = colSum.length;
+        while (i < n && j < m) {
+            result[i][j] = Math.min(rowSum[i], colSum[j]);
+            rowSum[i] = rowSum[i] - result[i][j];
+            colSum[j] = colSum[j] - result[i][j];
+            
+            if (rowSum[i] == 0) {
+                i++;
+            } else {
+                j++;
             }
         }
         return result;
