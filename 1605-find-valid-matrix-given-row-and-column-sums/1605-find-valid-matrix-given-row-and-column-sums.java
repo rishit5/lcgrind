09095256f48b7e24233cfaa464d08904c1144633
Rishit Ratna <rishitratan5@gmail.com>
2024-07-19 23:33:58 -0700

    Time: 1 ms (100.00%), Space: 53.4 MB (6.77%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -2,9 +2,7 @@ class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int[][] result = new int[rowSum.length][colSum.length];
         int i = 0, j = 0;
-        int n = rowSum.length;
-        int m = colSum.length;
-        while (i < n && j < m) {
+        while (i < rowSum.length && j < colSum.length) {
             result[i][j] = Math.min(rowSum[i], colSum[j]);
             rowSum[i] = rowSum[i] - result[i][j];
             colSum[j] = colSum[j] - result[i][j];
