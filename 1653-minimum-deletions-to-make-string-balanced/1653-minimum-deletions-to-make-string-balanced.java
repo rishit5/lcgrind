c52631739b156e3d475f1f124f5b4b1b89a509ab
Rishit Ratna <rishitratan5@gmail.com>
2024-07-30 10:14:57 -0700

    Time: 250 ms (5.29%), Space: 57.4 MB (5.01%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int[][] counts_back = new int[s.length()][2];
+        int[][] counts = new int[s.length()][2];
+        for (int i = 0; i <s.length(); i++) {
+            Arrays.fill(counts[i], 0);
+            Arrays.fill(counts_back[i], 0);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0) {
+                if (s.charAt(i) == 'a') {
+                    counts[i][0] = 1;
+                }
+                if (s.charAt(i) == 'b') {
+                    counts[i][1] = 1;
+                }  
+            } else {
+                if (s.charAt(i) == 'a') {
+                    counts[i][0] = counts[i-1][0] + 1;
+                    counts[i][1] = counts[i-1][1];
+                }
+                if (s.charAt(i) == 'b') {
+                    counts[i][1] = counts[i-1][1] + 1;
+                    counts[i][0] = counts[i-1][0];
+                }   
+            }
+        }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (i == s.length() - 1) {
+                if (s.charAt(i) == 'a') {
+                    counts_back[i][0] = 1;
+                }
+                if (s.charAt(i) == 'b') {
+                    counts_back[i][1] = 1;
+                }  
+            } else {
+                if (s.charAt(i) == 'a') {
+                    counts_back[i][0] = counts_back[i+1][0] + 1;
+                    counts_back[i][1] = counts_back[i+1][1];
+                }
+                if (s.charAt(i) == 'b') {
+                    counts_back[i][1] = counts_back[i+1][1] + 1;
+                    counts_back[i][0] = counts_back[i+1][0];
+                }   
+            }
+        }
+        int max = -1;
+        for (int i = 0; i < s.length(); i++) {
+            max = Math.max(counts[i][0] + counts_back[i][1], max);  
+            // System.out.println(counts[i][0] + " " + counts_back[i][1]);
+        }
+        return s.length() - max;
+    }
+}
