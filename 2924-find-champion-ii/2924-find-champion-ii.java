aeb59b5577a8574fad0d8f1ce5d4d0e11c49db45
Rishit Ratna <rishitratan5@gmail.com>
2024-11-25 20:59:28 -0700

    Time: 9 ms (14.29%), Space: 46.3 MB (5.15%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.java
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -1,8 +1,5 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        // [0, 1, 2] [[0, 1], [1, 2]] => [1] => [1, 2]
-        // {0:0, 1:0, 2:2, 3:1}
-        // {0:0, 1:0, 3:1, 3:1}
         if (n == 1) {
             return 0;
         }
@@ -14,18 +11,16 @@ class Solution {
             map.put(i, 0);
         }
         for (int i = 0; i < edges.length; i++) {
-            map.put(edges[i][1], map.getOrDefault(edges[i][1], 0) + 1);
+            map.put(edges[i][1], map.get(edges[i][1]) + 1);
         }
         PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         for (Integer num : map.keySet()) {
             pq.add(new Pair(num, map.get(num)));
         }
-        // System.out.println(map);
-        // System.out.println(pq);
+
         if (!pq.isEmpty()) {
             Pair<Integer, Integer> pair = pq.poll();
             Integer maxSo = pair.getValue();
-            // System.out.println(pq);
             if (!pq.isEmpty()) {
                 Pair<Integer, Integer> pair1 = pq.poll();
                 Integer maxSo1 = pair1.getValue();
