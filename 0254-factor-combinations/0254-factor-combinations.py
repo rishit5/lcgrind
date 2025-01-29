cd7f51d8861a6a87804e992310cb0b12cc75dfe3
Rishit Ratna <rishitratan5@gmail.com>
2025-01-29 16:16:04 -0700

    Time: 51 ms (37.29%), Space: 18.6 MB (50.69%) - LeetHub

--- 0254-factor-combinations/0254-factor-combinations.py
+++ 0254-factor-combinations/0254-factor-combinations.py
@@ -3,21 +3,17 @@ class Solution:
         self.result = []
     
     def dfs(self, curr, n, candidates, currList):
-        # print(n, candidates[curr], candidates, currList)
         if n % candidates[curr] == 0:
             currList.append(candidates[curr])
             currList.append(int(n / candidates[curr]))
             self.result.append(currList.copy())
             currList.pop()
             newCandidates = [x for x in range(candidates[curr], floor(sqrt(n / candidates[curr]))+1)]
-            # print(newCandidates)
             for i in range(len(newCandidates)):
                 self.dfs(i, int(n / candidates[curr]), newCandidates, currList)
             currList.pop()
 
     def getFactors(self, n: int) -> List[List[int]]:
-        # if n == 32:
-        #     return [[2,16],[4,8],[2,2,8],[2,4,4],[2,2,2,4],[2,2,2,2,2]]
         candidates = [x for x in range(2, floor(sqrt(n))+1)]
         for i in range(len(candidates)):
             self.dfs(i, n, candidates, [])
