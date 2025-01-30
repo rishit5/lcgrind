aa22d378c6791ce674ee769bcdaaeae7e9369da7
Rishit Ratna <rishitratan5@gmail.com>
2025-01-29 21:05:18 -0700

    Time: 2 ms (6.98%), Space: 17.8 MB (42.94%) - LeetHub

---
+++ 0156-binary-tree-upside-down/0156-binary-tree-upside-down.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None or root.left is None:
+            return root
+        newNode = self.upsideDownBinaryTree(root.left)
+        root.left.left = root.right
+        root.left.right = root
+        root.left = None
+        root.right = None
+        return newNode
