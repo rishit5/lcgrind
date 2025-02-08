7fcd09248c27cfce2d3d063f2d2b5c5e041b1e30
Rishit Ratna <rishitratan5@gmail.com>
2025-02-08 00:43:48 -0700

    Time: 0 ms (100%), Space: 41 MB (5.31%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,15 +1,5 @@
 class Solution {
     public int bulbSwitch(int n) {
-        long nLong = n;
-        int count = 0;
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-            long res = (long) i * (long) i;
-            if (res <= nLong) {
-                count += 1;
-            } else {
-                break;
-            }
-        }
-        return count;
+        return (int) Math.floor(Math.sqrt(n));
     }
 }
