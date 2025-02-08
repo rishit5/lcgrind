5df44da7c11941835468914e4cb92506074a8773
Rishit Ratna <rishitratan5@gmail.com>
2025-02-07 23:53:04 -0700

    Time: 1 ms (3.92%) | Memory: 40.5 MB (35.04%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        long nLong = n;
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            long res = (long) i * (long) i;
+            if (res <= nLong) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
