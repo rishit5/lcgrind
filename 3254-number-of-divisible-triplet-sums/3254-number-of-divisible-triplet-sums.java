3f7fdd303d053ac51b498bd1319838cb3edee6fe
Rishit Ratna <rishitratan5@gmail.com>
2025-02-10 11:25:13 -0700

    Time: 76 ms (62.12%), Space: 46.4 MB (29.29%) - LeetHub

--- 3254-number-of-divisible-triplet-sums/3254-number-of-divisible-triplet-sums.java
+++ 3254-number-of-divisible-triplet-sums/3254-number-of-divisible-triplet-sums.java
@@ -1,21 +1,21 @@
 class Solution {
     public int divisibleTripletCount(int[] nums, int d) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        // i, j, k
         int n = nums.length;
-        var map = new HashMap<Integer, Integer>();  // Stores frequency of remainder sums
         int ans = 0;
-
-        for (int i = 2; i < n; i++) {  // Fixing k as nums[i]
-            int prev = nums[i - 1] % d;  // Remainder of nums[k-1]
-
-            // Iterate over all possible nums[j] where j < k-1
-            for (int j = i - 2; j >= 0; j--) {  
-                int v = (nums[j] + prev) % d;  // Compute remainder sum of nums[j] and nums[k-1]
-                map.put(v, map.getOrDefault(v, 0) + 1);  // Store count of sum mod d
+        for (int k = 2; k < n; k++) {
+            int prev = nums[k-1] % d; // j
+            for (int i = k-2; i >= 0; i--) {
+                int value = (nums[i] + prev) % d;
+                countMap.put(value, countMap.getOrDefault(value, 0) + 1);
             }
-
-            // Check how many (i, j) pairs exist such that their sum complements nums[i] to be divisible by d
-            int v = nums[i] % d;
-            ans += (v == 0) ? map.getOrDefault(0, 0) : map.getOrDefault(d - v, 0);
+            int value = nums[k] % d;
+            if (value == 0) {
+                ans += countMap.getOrDefault(0, 0);
+            } else {
+                ans += countMap.getOrDefault(d - value, 0);
+            }
         }
         return ans;
     }
