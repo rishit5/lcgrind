52487a188ce02c6b204948a27ae02b19e766df16
Rishit Ratna <rishitratan5@gmail.com>
2025-03-01 09:34:50 -0700

    Time: 809 ms (8.20%) | Memory: 51.2 MB (86.80%) - LeetSync

---
+++ 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.java
@@ -0,0 +1,33 @@
+class TwoSum {
+
+    private TreeMap<Integer, Integer> treeMap;
+
+    public TwoSum() {
+        this.treeMap = new TreeMap<>();
+    }
+    
+    public void add(int number) {
+        this.treeMap.putIfAbsent(number, 0);
+        this.treeMap.put(number, treeMap.get(number) + 1);
+    }
+    
+    public boolean find(int value) {
+        System.out.println("Value is " + value);
+        for (Integer key : this.treeMap.keySet()) {
+            this.treeMap.put(key, this.treeMap.get(key) - 1);
+            if (this.treeMap.containsKey(value - key) && this.treeMap.get(value - key) > 0) {
+                this.treeMap.put(key, this.treeMap.get(key) + 1);
+                return true;
+            }
+            this.treeMap.put(key, this.treeMap.get(key) + 1);
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
