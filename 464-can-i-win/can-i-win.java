d1c35defe32811f0e93e5ad0d803f5e7bb15b368
Rishit Ratna <rishitratan5@gmail.com>
2025-03-11 00:37:23 -0700

    Time: 391 ms (49.15%) | Memory: 89.7 MB (41.62%) - LeetSync

--- 464-can-i-win/can-i-win.java
+++ 464-can-i-win/can-i-win.java
@@ -1,19 +1,16 @@
 class Solution {
     
-public boolean dfs(int maxChoosableInteger, int desiredTotal, Map<Integer, Boolean> dp, int key) {
+    public boolean dfs(int maxChoosableInteger, int desiredTotal, Map<Integer, Boolean> dp, int key) {
         if (dp.containsKey(key)) {
             return dp.get(key);
         }
         if (desiredTotal <= 0) {
             return false;
         }
-        for (int i = 1; i <= maxChoosableInteger; i++) {
-            if ((key & (1 << i)) == 0) { // Check if the number i is available
-                int newKey = key | (1 << i); // Mark i as used
-                if (!dfs(maxChoosableInteger, desiredTotal - i, dp, newKey)) {
-                    dp.put(key, true);
-                    return true;
-                }
+        for (int i = 0; i < maxChoosableInteger; i++) {
+            if (((key & (1<<i)) == 0) && !dfs(maxChoosableInteger, desiredTotal - i - 1, dp, key|1<<i)) {
+                dp.put(key, true);
+                return true;
             }
         }
         dp.put(key, false);
@@ -21,20 +18,20 @@ public boolean dfs(int maxChoosableInteger, int desiredTotal, Map<Integer, Boole
     }
 
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        int maxSum = ((maxChoosableInteger) * (maxChoosableInteger + 1)) / 2;
+        if (desiredTotal > maxSum) {
+            return false;
+        }
         if (desiredTotal <= 1) {
             return true;
-        }
-        int maxSum = (maxChoosableInteger * (maxChoosableInteger + 1)) / 2;
-        if (maxSum < desiredTotal) {
-            return false;
         }
         if (maxChoosableInteger >= desiredTotal) {
             return true;
         }
-        if (maxSum == desiredTotal) {
-            return maxChoosableInteger % 2 == 1;
+        if (desiredTotal == maxSum) {
+            return (maxChoosableInteger % 2 == 1);
         }
         Map<Integer, Boolean> dp = new HashMap<>();
-        return dfs(maxChoosableInteger, desiredTotal, dp, 0);
+        return dfs(maxChoosableInteger, desiredTotal, dp, 0);    
     }
 }
