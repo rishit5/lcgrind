9ee6dd0cfe19506b817a4da6412ca88be9b161a9
Rishit Ratna <rishitratan5@gmail.com>
2025-03-19 12:17:06 -0700

    Time: 1 ms (53.34%), Space: 41.4 MB (82.45%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,24 +1,27 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int openPara = 0;
-        int closePara = 0;
+        int closeBrac = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                openPara += 1;
+                closeBrac += 1;
             } else {
-                if (openPara > 0) {
-                    openPara -= 1;
-                }
+                if (closeBrac != 0) {
+                    closeBrac -= 1;
+                } 
             }
+        }
 
-            if (s.charAt(s.length() - i - 1) == ')') {
-                closePara += 1;
+        int openBrac  = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')') {
+                openBrac += 1;
             } else {
-                if (closePara > 0) {
-                    closePara -= 1;
+                if (openBrac != 0) {
+                    openBrac -= 1;
                 }
             }
         }
-        return closePara + openPara;
+
+        return closeBrac + openBrac;
     }
 }
