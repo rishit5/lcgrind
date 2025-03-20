7789bf6ae0b4a8abdfff7c48c08669447e499f24
Rishit Ratna <rishitratan5@gmail.com>
2025-03-19 19:15:05 -0700

    Time: 11 ms (90.89%), Space: 63.9 MB (51.99%) - LeetHub

--- 2423-minimum-deletions-to-make-array-divisible/2423-minimum-deletions-to-make-array-divisible.java
+++ 2423-minimum-deletions-to-make-array-divisible/2423-minimum-deletions-to-make-array-divisible.java
@@ -1,18 +1,28 @@
 class Solution {
+    
+    private int computeGCD(int x, int y) {
+        int large = x >= y ? x : y;
+        int small = x <  y ? x : y;
+        if (large % small == 0) {
+            return small;
+        } else {
+            return computeGCD(large % small, small);
+        }
+    }
+
     public int minOperations(int[] nums, int[] numsDivide) {
-        int g = numsDivide[0];
-        int tmp; 
-        for (int a : numsDivide) {
-            while (a > 0) {
-                tmp = g % a;
-                g = a;
-                a = tmp;
-            }
+        int gc = numsDivide[0];
+        for (int i = 1; i < numsDivide.length; i++) {
+            gc = computeGCD(gc, numsDivide[i]);
         }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length && nums[i] <= g; i++) {
-            if (g % nums[i] == 0) {
-                return i;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > gc) {
+                return -1;
+            } else {
+                if (gc % nums[i] == 0) {
+                    return i;
+                }
             }
         }
         return -1;
