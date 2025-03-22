44a1fcbb1f5273d6c1c21df9de9d61e19a8dbb35
Rishit Ratna <rishitratan5@gmail.com>
2025-03-21 22:50:42 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -2,37 +2,48 @@ class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = 0;
         int right = arr.length - 1;
-        while (left < right) {
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] > x) {
-                right = mid;
-            } else if (arr[mid] < x) {
+            if (Math.abs(arr[mid] - x) < min) {
+                min = Math.abs(arr[mid] - x);
+                index = mid;
+            }
+            if (min == 0) {
+                right = mid - 1;
+            } else if (arr[mid] > x) {
+                right = mid - 1;
+            } else {
                 left = mid + 1;
-            } else if (arr[mid] == x) {
-                right = mid;
             }
         }
+
         List<Integer> result = new LinkedList<>();
-        int l = left - 1;
-        int r = left;
-        // System.out.println(left);
-        while (k-- > 0) {
+        result.addLast(arr[index]);
+        k -= 1;
+        int l = index - 1;
+        int r = index + 1;
+        while (k > 0) {
             if (l >= 0 && r < arr.length) {
-                if (Math.abs(x - arr[l]) <= Math.abs(x - arr[r])) {
+                if (Math.abs(arr[l] - x) <= Math.abs(arr[r] - x)) {
                     result.addFirst(arr[l]);
                     l -= 1;
                 } else {
                     result.addLast(arr[r]);
-                    r += 1;
+                    r += 1; 
                 }
             } else if (l >= 0) {
                 result.addFirst(arr[l]);
                 l -= 1;
-            } else if (r < arr.length) {
+            } else {
                 result.addLast(arr[r]);
                 r += 1;
             }
+            k -= 1;
         }
+
         return result;
     }
 }
