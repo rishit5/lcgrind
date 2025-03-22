e5c7549f945f90b8a791573595f0bd45fe0ebf1e
Rishit Ratna <rishitratan5@gmail.com>
2025-03-21 21:28:56 -0700

    Time: 1 ms (81.61%), Space: 41.4 MB (91.12%) - LeetHub

--- 0961-long-pressed-name/0961-long-pressed-name.java
+++ 0961-long-pressed-name/0961-long-pressed-name.java
@@ -1,42 +1,35 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        List<Pair<Character, Integer>> list = new LinkedList<>();
-        Character currChar = name.charAt(0);
-        int count = 1;
-        for (int i = 1; i < name.length(); i++) {
-            if (name.charAt(i) != name.charAt(i-1)) {
-                list.addLast(new Pair(currChar, count));
-                currChar = name.charAt(i);
-                count = 1;
-            } else {
-                count += 1;
-            }
-        }
-        list.addLast(new Pair(currChar, count));
-        List<Pair<Character, Integer>> listType = new LinkedList<>();
-        currChar = typed.charAt(0);
-        count = 1;
-        for (int i = 1; i < typed.length(); i++) {
-            if (typed.charAt(i) != typed.charAt(i-1)) {
-                listType.addLast(new Pair(currChar, count));
-                currChar = typed.charAt(i);
-                count = 1;
-            } else {
-                count += 1;
-            }
-        }
-        listType.addLast(new Pair(currChar, count));
-        if (list.size() != listType.size()) {
+        int i = 0;
+        int j = 0;
+        if (typed.length() < name.length()) {
             return false;
         }
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getKey() != listType.get(i).getKey()) {
-                return false;
-            } 
-            if (list.get(i).getValue() > listType.get(i).getValue()) {
-                return false;
+        while (i < name.length() && j < typed.length()) {
+            if (name.charAt(i) == typed.charAt(j)) {
+                i += 1;
+                j += 1;
+            } else {
+                if (i == 0) {
+                    return false;
+                } else {
+                    while (j < typed.length() && typed.charAt(j) == typed.charAt(j-1)) {
+                        j += 1;
+                    }
+                    if (j < typed.length() && name.charAt(i) != typed.charAt(j)) {
+                        return false;
+                    }
+                }
             }
         }
-        return true;
+        // System.out.println(i);
+        // System.out.println(j);
+        while (j < typed.length()) {
+            if (name.charAt(i-1) != typed.charAt(j)) {
+                return false;
+            }
+            j += 1;
+        }
+        return (i == name.length());
     }
 }
