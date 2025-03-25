c34422937a08716492e62d0cdab0ef0a38cae66b
Rishit Ratna <rishitratan5@gmail.com>
2025-03-24 17:34:12 -0700

    Time: 13 ms (6.03%) | Memory: 45 MB (16.09%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,19 +1,29 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordSet = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length()];
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                String sub = s.substring(j, i);
-                if (j == 0 && wordSet.contains(sub)) {
-                    dp[i-1] = true;
-                    break;
-                } else if (j >= 1 && dp[j-1] && wordSet.contains(sub)) {
-                    dp[i-1] = true;
-                    break;
+    int[] memo;
+    private boolean backTrack(String s, int curr, Set<String> wordDict) {
+        if (curr == s.length()) {
+            return true;
+        }
+        if (this.memo[curr] != -1) {
+            return this.memo[curr] == 1;
+        }
+        String currString = "";
+        for (int i = curr; i < s.length(); i++) {
+            currString += s.charAt(i);
+            if (wordDict.contains(currString)) {
+                this.memo[curr] = 1;
+                if (this.backTrack(s, i + 1, wordDict)) {
+                    return true;
                 }
             }
         }
-        return dp[s.length() - 1];
+        this.memo[curr] = 0;
+        return false;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        this.memo = new int[s.length()];
+        Arrays.fill(this.memo, -1);
+        return backTrack(s, 0, new HashSet<>(wordDict));
     }
 }
