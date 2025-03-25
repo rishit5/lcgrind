5f9f028a803a92070c11bff65a882c7b27312cc9
Rishit Ratna <rishitratan5@gmail.com>
2025-03-24 23:06:51 -0700

    Time: 66 ms (68.47%), Space: 114.7 MB (94.6%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.java
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.java
@@ -1,46 +1,46 @@
 class Solution {
+  public long maxSubarrays(int n, int[][] conflictingPairs) {
+    long validSubarrays = 0;
+    int maxLeft = 0;
+    int secondMaxLeft = 0;
+    // gains[i] := the number of additional valid subarrays we can gain if the
+    // restriction at index `i` is removed
+    long[] gains = new long[n + 1];
+    // conflicts[r] := left endpoints that conflict with subarrays ending in r
+    List<Integer>[] conflicts = new List[n + 1];
 
+    for (int i = 0; i <= n; ++i)
+      conflicts[i] = new ArrayList<>();
 
-    private int[] max(int[] a, int[] b, int[] c) {
-        if (a[0] > b[0] || (a[0] == b[0] && a[1] > b[1])) {
-            if (a[0] > c[0] || (a[0] == c[0] && a[1] > c[1])) {
-                return a; // a is the largest
-            }
-        }
-        if (b[0] > c[0] || (b[0] == c[0] && b[1] > c[1])) {
-            return b;
-        }
-        return c;
+    for (int[] pair : conflictingPairs) {
+      final int a = pair[0];
+      final int b = pair[1];
+      conflicts[Math.max(a, b)].add(Math.min(a, b));
     }
 
-    public long maxSubarrays(int n, int[][] conflictingPairs) {
-        List<List<Integer>> right = new ArrayList<>();
-        
-        for (long i = 0; i <= n; i++) {
-            right.add(new ArrayList<>());
+    for (int right = 1; right <= n; ++right) {
+      for (int left : conflicts[right]) {
+        if (left > maxLeft) {
+          secondMaxLeft = maxLeft;
+          maxLeft = left;
+        } else if (left > secondMaxLeft) {
+          secondMaxLeft = left;
         }
-        
-        for (int i = 0; i < conflictingPairs.length; i++) {
-            right.get(Math.max(conflictingPairs[i][0], conflictingPairs[i][1]))
-                .add(Math.min(conflictingPairs[i][0], conflictingPairs[i][1]));
-        }
-
-        long ans = 0;
-        int[] left = new int[]{0, 0};
-        long[] imp = new long[n+1];
-        long maxSoFar = Long.MIN_VALUE;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j : right.get(i)) {
-                left = max(left, new int[]{j, left[0]}, new int[]{left[0], j});
-            }
-            ans += i - left[0];
-            imp[left[0]] += (long) left[0] - (long) left[1];
-            if (maxSoFar < imp[left[0]]) {
-                maxSoFar = imp[left[0]];
-            }
-        }
-
-        return ans + maxSoFar;
+      }
+      // Subarrays [maxLeft + 1..right],
+      //           [maxLeft + 2..right],
+      //           ...
+      //           [right..right] are valid.
+      validSubarrays += right - maxLeft;
+      // If we remove `maxLeft` (the most restrictive conflict), we gain
+      // `maxLeft - secondMaxLeft` new subarrays:
+      // [secondMaxLeft + 1..right],
+      // [secondMaxLeft + 2..right],
+      // ...
+      // [maxLeft..right].
+      gains[maxLeft] += maxLeft - secondMaxLeft;
     }
+
+    return validSubarrays + Arrays.stream(gains).max().getAsLong();
+  }
 }
