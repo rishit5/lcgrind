ac2a521bb5b2a2b83883c9dc55408867fb09240f
Rishit Ratna <rishitratan5@gmail.com>
2025-04-21 18:27:03 -0700

    Time: 1 ms (92.75%) | Memory: 41.5 MB (85.25%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String reorganizeString(String s) {
+        int[] sCharArray = new int[26];
+        int maxCount = Integer.MIN_VALUE;
+        int maxLetter = -1;
+        for (int i = 0; i < s.length(); i++) {
+            sCharArray[s.charAt(i) - 'a'] += 1;
+            if (sCharArray[s.charAt(i) - 'a'] > maxCount) {
+                maxCount = sCharArray[s.charAt(i) - 'a'];
+                maxLetter = s.charAt(i) - 'a';
+            }
+        }
+        // System.out.println(maxCount);
+        if (maxCount > (s.length() + 1) / 2) {
+            return "";
+        }
+        int i = 0;
+        char[] result = new char[s.length()];
+        while (i < s.length() && sCharArray[maxLetter] > 0) {
+            result[i] = (char) (maxLetter + 'a');
+            sCharArray[maxLetter] -= 1;
+            i += 2;
+        }
+        for (int j = 0; j < 26; j++) {
+            while (sCharArray[j] > 0) {
+                if (i >= s.length()) {
+                    i = 1;
+                }
+                result[i] = (char) (j + 'a');
+                sCharArray[j] -= 1;
+                i += 2;
+            }
+        }
+        return new String(result);
+    }
+}
